2d0ec6f087869b257309ad800ce7702ef7f2b811
Pawel Marcinkowski <pawel.marcinkowski@allegropay.pl>
2024-12-29 23:28:17 +0100

    gsawon aggregation

--- src/divide_and_conquer_sentiment/aggregation/sawon.py
+++ src/divide_and_conquer_sentiment/aggregation/sawon.py
@@ -1,19 +1,16 @@
-from dataclasses import dataclass
 from .base import AggregatorBase
 import torch
-from divide_and_conquer_sentiment import SentimentModel
+from transformers import TextClassificationPipeline
 
-@dataclass()
 class SawonAggregator(AggregatorBase):
 
-    def __init__(self, sentiment_model: SentimentModel, treshold: float = 0.9):
-
+    def __init__(self, passages: list[str], sentiment_model: TextClassificationPipeline, treshold: float = 0.9):
+        self.passages = passages
         self.sentiment_model = sentiment_model
         self.treshold = treshold
 
-    def aggregate(self, subpredictions: list[torch.Tensor] , **kwargs) -> list[torch.Tensor]:
-        passages = kwargs['passages']
-        defaults =  self.sentiment_model.predict(passages)
+    def aggregate(self, subpredictions: list[torch.Tensor]) -> list[torch.Tensor]:
+        defaults = self.full_passage_prediction()
         result = []
         for i in range(len(subpredictions)):
             scores_array = subpredictions[i]
@@ -21,8 +18,11 @@ class SawonAggregator(AggregatorBase):
             result.append(self.awon(scores_array, default))
         return result
 
+    def full_passage_prediction(self) -> list[torch.Tensor]:
+        return self.sentiment_model.predict(self.passages)
+
     def awon(self, scores_array: torch.Tensor, default) -> torch.Tensor:
-        if scores_array.shape[0] == 1:
+        if scores_array.dim() == 1:
             return default
         mask = scores_array[:, 1] <= self.treshold
         if max(mask) == False:
--- tests/test_aggregation/test_sawon.py
+++ tests/test_aggregation/test_sawon.py
@@ -2,8 +2,8 @@ from unittest import mock
 
 import pytest
 import torch
-from divide_and_conquer_sentiment import SentimentModel
-from divide_and_conquer_sentiment.aggregation.sawon import SawonAggregator
+from transformers import pipeline
+from src.divide_and_conquer_sentiment.aggregation.sawon import SawonAggregator
 
 # Helper function to assert lists of tensors
 def assert_tensor_lists_equal(list1, list2):
@@ -13,16 +13,27 @@ def assert_tensor_lists_equal(list1, list2):
 
 @pytest.fixture
 def mocks(mocker):
-    mock_model = mocker.Mock(spec=SentimentModel)
-    mock_model.predict.return_value = [torch.zeros(3), torch.zeros(3), torch.zeros(3)]
-    sawon = SawonAggregator(mock_model)
+    mock_pipeline = mocker.Mock(spec=pipeline)
+    mock_pipeline._postprocess_params = {"top_k": None}
+    passages = [""]
+    sawon = SawonAggregator(passages, mock_pipeline)
 
+    mock_tensor = [torch.zeros(3), torch.zeros(3), torch.zeros(3)]
+    # Mock `my_function` to return `mock_tensor`
+    mocker.patch.object(sawon,'full_passage_prediction', return_value=mock_tensor)
     return sawon
+
+def test_full_passage_prediction(mocks):
+    expected_tensor = [torch.zeros(3), torch.zeros(3), torch.zeros(3)]
+    result = mocks.full_passage_prediction()
+
+    assert_tensor_lists_equal(result, expected_tensor), "The tensors do not match!"
+
 
 @pytest.mark.parametrize(
     "input, expected",
     [(torch.ones((5, 3)), torch.zeros(3))
-        , (torch.ones(1, 3)/2, torch.zeros(3))
+        , (torch.ones(3), torch.zeros(3))
         , (torch.ones((5, 3))/2, torch.ones(3)/2)
         ,( torch.tensor([[0.10, 0.91, 0.00],[0.9000, 0.1000, 0.0000]]), torch.tensor([0.9000, 0.1000, 0.0000]) )],
 )
@@ -36,10 +47,10 @@ def test_awon(input, expected, mocks):
 
 @pytest.mark.parametrize(
     "subpredictions, expected",
-    [ ([torch.ones((5, 3)), torch.ones(1,3), torch.ones((5, 3))/2] , [torch.zeros(3),torch.zeros(3),torch.ones(3)/2])
+    [ ([torch.ones((5, 3)), torch.ones(3), torch.ones((5, 3))/2] , [torch.zeros(3),torch.zeros(3),torch.ones(3)/2])
      ]
 )
 def test_aggregate(subpredictions, expected, mocks):
     sawon =mocks
-    output = sawon.aggregate(subpredictions, passages = [])
+    output = sawon.aggregate(subpredictions)
     assert_tensor_lists_equal(output, expected)
